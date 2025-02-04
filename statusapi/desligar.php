0a838a0186b8d42318bf5acd4ab4126c4011a00d
João Victor Julião <joaovictojuliao@hotmail.com>
2025-02-04 06:25:35 -0300

    statusapi.css

--- statusapi/desligar.php
+++ statusapi/desligar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../statusapi.css">
 </head>
 <body>
     <header>
